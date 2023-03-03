7ebfd6be4a52bba7b9c5247692422c4ec41988bf
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2023-03-03 15:02:24 -0300

    [FRONT] | Feat: Getting data from the local API and setting the values

--- via-cep/src/App.js
+++ via-cep/src/App.js
@@ -3,20 +3,28 @@ import { FiSearch } from 'react-icons/fi';
 import './css/styles.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import api from './services/api';
 
 function App() {
 
   const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
 
-  const handleSearch = () => {
-    if (input === '') {
+  async function handleSearch() {
+    try {
+      const response = await api.get(`/address/find-cep/${input}`);
+      setCep(response.data);
+      setInput("");
+    } catch (error) {
       toast('Digite um CEP!', {
         position: toast.POSITION.BOTTOM_RIGHT,
         className: 'toast-message',
         autoClose: 2000
       });
+      setInput("");
     }
   }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -37,11 +45,11 @@ function App() {
         </button>
       </div>
       <main className='main'>
-        <h2>CEP:95538-000</h2>
-        <span>Logradouro:-</span>
-        <span>Municipio: Itati</span>
-        <span>UF: RS</span>
-        <span>bairro: bairro fim</span>
+        <h2>CEP:{cep.cep ? "-" : cep.cep}</h2>
+        <span>Logradouro:{!cep.publicPlace ? "-" : cep.publicPlace}</span>
+        <span>Municipio: {!cep.county ? "-" : cep.county}</span>
+        <span>UF:{!cep.uf ? "-" : cep.uf}</span>
+        <span>bairro:{!cep.neighborhood ? "-" : cep.neighborhood}</span>
       </main>
       <ToastContainer />
     </div>
